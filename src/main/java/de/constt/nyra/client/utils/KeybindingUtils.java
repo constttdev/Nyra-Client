cde0647107897c81f56b2432cf01a4c989c47d63
constt <175602385+constttdev@users.noreply.github.com>
2026-08-15 15:30:07 +0200

    feat: keybinding utils feat: get key name function

--- src/main/java/de/constt/nyra/client/utils/KeybindingUtils.java
+++ src/main/java/de/constt/nyra/client/utils/KeybindingUtils.java
@@ -15,4 +15,62 @@ public class KeybindingUtils {
         else if (keybindArg.equals("SHIFT")) keyCode = GLFW.GLFW_KEY_LEFT_SHIFT;
         return keyCode;
     }
+
+    public static String getKeyName(int keyCode, boolean showNone) {
+        if (keyCode == -1 && showNone) {
+            return "NONE";
+        }
+
+        if (keyCode == GLFW.GLFW_KEY_UNKNOWN || keyCode == -1) {
+            return "UNKNOWN";
+        }
+
+        String name = GLFW.glfwGetKeyName(keyCode, 0);
+
+        if (name != null) {
+            return name.toUpperCase();
+        }
+
+        return switch (keyCode) {
+            case GLFW.GLFW_KEY_SPACE -> "SPACE";
+            case GLFW.GLFW_KEY_ENTER -> "ENTER";
+            case GLFW.GLFW_KEY_ESCAPE -> "ESC";
+            case GLFW.GLFW_KEY_TAB -> "TAB";
+            case GLFW.GLFW_KEY_BACKSPACE -> "BACKSPACE";
+            case GLFW.GLFW_KEY_INSERT -> "INSERT";
+            case GLFW.GLFW_KEY_DELETE -> "DELETE";
+            case GLFW.GLFW_KEY_RIGHT -> "RIGHT";
+            case GLFW.GLFW_KEY_LEFT -> "LEFT";
+            case GLFW.GLFW_KEY_DOWN -> "DOWN";
+            case GLFW.GLFW_KEY_UP -> "UP";
+            case GLFW.GLFW_KEY_PAGE_UP -> "PAGE UP";
+            case GLFW.GLFW_KEY_PAGE_DOWN -> "PAGE DOWN";
+            case GLFW.GLFW_KEY_HOME -> "HOME";
+            case GLFW.GLFW_KEY_END -> "END";
+            case GLFW.GLFW_KEY_CAPS_LOCK -> "CAPS LOCK";
+            case GLFW.GLFW_KEY_SCROLL_LOCK -> "SCROLL LOCK";
+            case GLFW.GLFW_KEY_NUM_LOCK -> "NUM LOCK";
+            case GLFW.GLFW_KEY_PRINT_SCREEN -> "PRINT SCREEN";
+            case GLFW.GLFW_KEY_PAUSE -> "PAUSE";
+            case GLFW.GLFW_KEY_LEFT_SHIFT -> "LEFT SHIFT";
+            case GLFW.GLFW_KEY_LEFT_CONTROL -> "LEFT CTRL";
+            case GLFW.GLFW_KEY_LEFT_ALT -> "LEFT ALT";
+            case GLFW.GLFW_KEY_LEFT_SUPER -> "LEFT SUPER";
+            case GLFW.GLFW_KEY_RIGHT_SHIFT -> "RIGHT SHIFT";
+            case GLFW.GLFW_KEY_RIGHT_CONTROL -> "RIGHT CTRL";
+            case GLFW.GLFW_KEY_RIGHT_ALT -> "RIGHT ALT";
+            case GLFW.GLFW_KEY_RIGHT_SUPER -> "RIGHT SUPER";
+            default -> {
+                if (keyCode >= GLFW.GLFW_KEY_F1 && keyCode <= GLFW.GLFW_KEY_F25) {
+                    yield "F" + (keyCode - GLFW.GLFW_KEY_F1 + 1);
+                }
+
+                if (keyCode >= GLFW.GLFW_KEY_0 && keyCode <= GLFW.GLFW_KEY_9) {
+                    yield String.valueOf((char) ('0' + keyCode - GLFW.GLFW_KEY_0));
+                }
+
+                yield "UNKNOWN";
+            }
+        };
+    }
 }
